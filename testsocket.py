315824ad4b7b88faa8c11af61484cc73e08ba7a0
Halász András <halasz.andras.94@gmail.com>
2019-04-24 23:15:45 +0200

    fölös fájlok

--- testsocket.py
+++
@@ -1,9 +0,0 @@
-import socket
-from game import *
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 12321))
-
-comm=Communicator(s)
-comm.print('Ping\r\n')
-s=comm.read_line()
-print(s)
